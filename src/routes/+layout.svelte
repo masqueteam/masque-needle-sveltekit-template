1310b257b26605c8c8a48585eebe48e94be29031
masqueteam <masqueteam.2022@gmail.com>
2025-05-22 10:44:26 +0700

    fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,22 +1,18 @@
 <script lang="ts">
   import FixedScaleLayout from "$lib/components/FixedScaleLayout.svelte";
-    import { onMount } from "svelte";
   import "../app.css";
   let { children } = $props();
-  onMount(() => {
-    console.log("NeedleEngine loaded");
-  });
 </script>
 
 <FixedScaleLayout>
-  <div
-    class="absolute inset-0 flex justify-center items-center"
-  >
-    {#await import("$lib/components/NeedleEngine.svelte")}
-      <div class="absolute text-gl text-white animate-pulse">Loading...</div>
-    {:then { default: NeedleEngine }}
-      <NeedleEngine />
+  {#await import("$lib/components/NeedleEngine.svelte")}
+    <div class="absolute inset-0 flex justify-center items-center animate-pulse">
+      Loading...
+    </div>
+  {:then { default: NeedleEngine }}
+    <NeedleEngine />
+    <div class="absolute inset-0 flex justify-center items-center pointer-events-none">
       {@render children()}
-    {/await}
-  </div>
+    </div>
+  {/await}
 </FixedScaleLayout>
